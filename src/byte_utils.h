17774c857ed189b5e403050dc66b1666d1fa4f9c
Arthur <arthurgargon@gmail.com>
2022-03-13 20:00:17 +0400

    fix

--- src/byte_utils.h
+++ src/byte_utils.h
@@ -9,12 +9,13 @@ int char2int(char input){
   if(input >= 'a' && input <= 'f')
     return input - 'a' + 10;
   //throw std::invalid_argument("Invalid input string");
+  return 0;
 }
 
 // This function assumes src to be a zero terminated sanitized string with
 // an even number of [0-9a-f] characters, and target to be sufficiently large
 void hex2bin(const char* src, char* target){
-  while(*src && src[1]){
+  while(*src && src[1]){ 
     *(target++) = char2int(*src)*16 + char2int(src[1]);
     src += 2;
   }
